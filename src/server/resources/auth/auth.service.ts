8a1ad20f8f5719c817a0deabf0b27b877969ab15
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-27 04:09:53 +0000

    feat: implement login and signup methods in AuthService with error handling

--- src/server/resources/auth/auth.service.ts
+++ src/server/resources/auth/auth.service.ts
@@ -1,14 +1,12 @@
 import prisma from "../../../../prisma/prisma";
-import { SignupDto } from "./auth.dto";
 import * as bcrypt from "bcrypt";
+import { LoginDto, SignUpDto } from "./auth.dto";
+import { TsRestResponseError } from "@ts-rest/core";
+import { contract } from "@/server/router/contract";
 
 export class AuthService {
-  static async login(data: SignupDto) {
+  static async login(data: LoginDto) {
     const { email, password } = data;
-
-    if (!email || !password) {
-      throw "Email e senha são obrigatórios.";
-    }
 
     const user = await prisma.user.findUnique({ where: { email } });
 
@@ -19,6 +17,30 @@ export class AuthService {
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) throw "Usuario ou senha incorretos";
+
+    return "ok";
+  }
+
+  static async signUp(data: SignUpDto) {
+    const { email, password } = data;
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (user) {
+      throw new TsRestResponseError(contract.auth.signUp, {
+        body: "Email já cadastrado.",
+        status: 409,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
 
     return "ok";
   }
