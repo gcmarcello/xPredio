fe58964be0a6b130ad71a5042b4ef7f9146116ca
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-27 04:10:19 +0000

    feat: add RESTful route handler for API methods

---
+++ src/app/api/[...ts-rest]/route.ts
@@ -0,0 +1,10 @@
+import { handler } from "@/server/router/handler";
+
+export {
+  handler as GET,
+  handler as POST,
+  handler as PUT,
+  handler as PATCH,
+  handler as DELETE,
+  handler as OPTIONS,
+};
