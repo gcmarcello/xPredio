2fa6bd4163248add392d3b9ee67e3c0ad6326534
Marcello Coelho <marcellogcoelho@outlook.com>
2024-11-27 04:09:45 +0000

    feat: update authentication DTOs for login and signup

--- src/server/resources/auth/auth.dto.ts
+++ src/server/resources/auth/auth.dto.ts
@@ -1,8 +1,15 @@
 import { z } from "@/utils/zod/zod";
 
-export const signupDto = z.object({
+export const loginDto = z.object({
   email: z.string().email(),
   password: z.string(),
 });
 
-export type SignupDto = z.infer<typeof signupDto>;
+export type LoginDto = z.infer<typeof loginDto>;
+
+export const signUpDto = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export type SignUpDto = z.infer<typeof signUpDto>;
